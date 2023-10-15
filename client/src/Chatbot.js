e68cbc07c5fff95e2e6708ae657a3bf584e63e55
Hong-ut <7594hsj@gmail.com>
2023-10-14 21:10:45 -0400

    finisehd chatbot

--- client/src/Chatbot.js
+++ client/src/Chatbot.js
@@ -29,7 +29,7 @@ const Chatbot = () => {
       if (i < userInputs.length) {
         newMessages.push(
           <div
-            className="text-white justify-center items-center w-5/12 break-words h-auto mr-10 self-end user-color rounded-3xl p-2 text-sm"
+            className="text-white justify-center items-center w-4/12 break-words h-auto mr-20 self-end user-color rounded-3xl p-5 text-sm"
             key={i}
           >
             {userInputs[i]}
@@ -39,7 +39,7 @@ const Chatbot = () => {
       if (i < responses.length) {
         newMessages.push(
           <div
-            className="text-white justify-center items-center break-words h-full w-5/12 ml-10 self-start ai-color rounded-3xl p-2"
+            className="text-black justify-center items-center break-words h-full w-4/12 ml-20 self-start ai-color rounded-3xl p-5"
             key={i}
           >
             {responses[i]}
@@ -51,31 +51,32 @@ const Chatbot = () => {
     setChatMessages(newMessages);
   }, [responses, userInputs]);
 
-  const callAPI = () => {
+  const callAPI = async () => {
     try {
-    console.log(prompt);
-    setUserInputs((prevInputs) => [...prevInputs, prompt]);
-    console.log(JSON.stringify({ prompt }));
-    const body = { prompt: prompt };
-
-    const response = fetch("http://127.0.0.1:5000/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-      },
-      body: JSON.stringify(body),
-    })
-        let resp = "";
-        response.json().then((data) => {
-          console.log(data);
-          resp = data.response;
+        setUserInputs((prevInputs) => [...prevInputs, prompt])
+        const body = { "prompt": prompt }; // convert to JSON since body needs to be in JSON format
+        // const responses = [];
+        const response = await fetch('http://127.0.0.1:5000/chat/', {
+            method: "POST",
+            // mode: 'no-cors',
+            headers: {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": '*',
+                "Access-Control-Allow-Headers": '*',
+                "Access-Control-Allow-Methods": 'GET, POST, PUT, DELETE'
+            },
+            body: JSON.stringify(body)
         });
+        // console.log(await response.json())
+        let resp = "";
+        await response.json().then((data) => {
+            console.log(data)
+            resp = data.response;
+        })
         setResponses((prevResponses) => [...prevResponses, resp]);
-    }
-    catch (error) {
+
+
+    } catch (error) {
         console.log(error);
     }
 
@@ -89,7 +90,7 @@ const Chatbot = () => {
     <div className="h-screen bg-black p-16 pt-25">
       <div className="flex flex-col rounded-3xl main-height">
         {/* Header */}
-        <div className="flex justify-center h-1/6 items-center text-white shadow-lg rounded-3xl header-color">
+        <div className="font-bold text-2xl flex justify-center h-1/6 items-center text-white shadow-lg rounded-3xl header-color">
           <h1>AI CHAT</h1>
         </div>
 
