fdf2c1d6237246a680296390badf5f012ffafaf5
Jon Vincentius <jon.vincentius@mail.utoronto.ca>
2023-10-15 03:26:58 -0400

    Improved Home page Start button

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CartCanvas from "./Cart";
 import { useNavigate } from 'react-router-dom';
+import "./Home.css";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -15,16 +16,20 @@ const Home = () => {
     <div className="h-screen flex items-center justify-center flex-row bg-zinc-900">
       <div className="text-white">
         <h1 className="text-8xl font-semibold">ShopWise</h1>
-        <h2 className="whitespace-normal max-w-lg mt-12 text-lg">
+        <h2 className="whitespace-normal max-w-lg mt-12 text-lg mb-8">
           Powered by ChatGPT, ShopWise streamlines your online shopping
           experience by distilling valuable insights and visual comparisons,
           empowering you to make better informed decisions for all your needs.
         </h2>
-        <button className="text-white font-semibold text-xl rounded-full py-2.5 px-[80px] bg-zinc-800 border-4 border-[#D5A13D] mt-8" 
+        {/* <button className="text-white font-semibold text-xl rounded-full py-2.5 px-[80px] bg-zinc-800 border-4 border-[#D5A13D] mt-8" 
         onClick={handleButtonClick}
         >
           START
-        </button>
+        </button> */}
+        <ul>
+          <li><a onClick={handleButtonClick} className="flex flex-row justify-center cursor-pointer"><span>START</span></a></li>
+        </ul>
+
       </div>
       <div className="h-screen"><CartCanvas /></div>
     </div>
