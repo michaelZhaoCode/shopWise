773c786bb611d66ae15fd8e95de98a0c975ebb0a
Hong-ut <7594hsj@gmail.com>
2023-10-14 22:01:27 -0400

    frontend

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 import CartCanvas from "./Cart";
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+
+
+  const handleButtonClick = () => {
+    // Redirect to the "/search" route
+    navigate('/search')
+  };
   return (
     <div className="h-screen flex items-center justify-center flex-row bg-zinc-900">
       <div className="text-white">
@@ -11,7 +20,9 @@ const Home = () => {
           experience by distilling valuable insights and visual comparisons,
           empowering you to make better informed decisions for all your needs.
         </h2>
-        <button className="text-white font-semibold text-xl rounded-full py-2.5 px-[80px] bg-zinc-800 border-4 border-[#D5A13D] mt-8">
+        <button className="text-white font-semibold text-xl rounded-full py-2.5 px-[80px] bg-zinc-800 border-4 border-[#D5A13D] mt-8" 
+        onClick={handleButtonClick}
+        >
           START
         </button>
       </div>
